4a349436c713239c365e8ccfa95a0fe7d98d9328
beakbreak <namanbhagat1414@gmail.com>
2022-10-17 00:15:11 +0530

    final website first report commit

--- website/dbms_project/lost_and_found/urls.py
+++ website/dbms_project/lost_and_found/urls.py
@@ -2,5 +2,10 @@ from nturl2path import url2pathname
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("",views.index, name="index")
+    path("",views.index, name="index"),
+    path("form/",views.itemform,name="form"),
+    path("item/",views.item,name="item"),
+    path("login/",views.login,name="login"),
+    path("category/",views.category,name="category"),
+    path("myreq/",views.myreq,name="myreq")
 ]
--- website/dbms_project/lost_and_found/views.py
+++ website/dbms_project/lost_and_found/views.py
@@ -3,3 +3,17 @@ from django.shortcuts import render
 def index(request):
     return render(request,"lost_and_found/index.html")
     
+def itemform(request):
+    return render(request,"lost_and_found/itemform.html")
+
+def item(request):
+    return render(request,"lost_and_found/item.html")
+
+def login(request):
+    return render(request,"lost_and_found/login.html")
+
+def category(request):
+    return render(request,"lost_and_found/category.html")
+
+def myreq(request):
+    return render(request,"lost_and_found/my_requests.html")
